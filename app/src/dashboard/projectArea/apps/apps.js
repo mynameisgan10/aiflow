3d4d0bb7dc35d5c0ad05c402950117d0d44b7503
gandeveloper <gan@Gans-New-Macbook-Pro.local>
2017-12-12 20:55:59 +0800

    outputing apps dynamically

--- app/src/dashboard/projectArea/apps/apps.js
+++ app/src/dashboard/projectArea/apps/apps.js
@@ -2,33 +2,19 @@ import React, { Component } from 'react';
 import App from './app/app';
 
 class Apps extends Component {
+
     render() {
+        const testValue = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
+        const output = testValue.map(value => {
+            return (
+                <div className="column is-2">
+                    <App />
+                </div>
+            )
+        });
         return (
             <div className="columns is-multiline">
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
-                <div className="column is-2">
-                    <App />
-                </div>
+                {output}
                 <div className="column is-2">
                     <h1 className="has-text-danger title">Add more</h1>
                 </div>
