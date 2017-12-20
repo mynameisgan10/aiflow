6882d913f48c3a81aee52f771205b18b66943b35
gandeveloper <gan@Gans-New-Macbook-Pro.local>
2017-12-20 14:38:09 +0700

    fixed class to className

--- app/src/dashboard/projectArea/apps/app/app.js
+++ app/src/dashboard/projectArea/apps/app/app.js
@@ -7,23 +7,23 @@ const App = (props) => {
         <Link to="/test">
             <div className="card" onClick={props.onClick}>
                 <div className="card-image">
-                    <figure class="image is-4by3">
+                    <figure className="image is-4by3">
                         <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
                     </figure>
                 </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-48x48">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-left">
+                            <figure className="image is-48x48">
                                 <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
                             </figure>
                         </div>
-                        <div class="media-content">
-                            <p class="title is-4">John Smith</p>
-                            <p class="subtitle is-6">@johnsmith</p>
+                        <div className="media-content">
+                            <p className="title is-4">John Smith</p>
+                            <p className="subtitle is-6">@johnsmith</p>
                         </div>
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h1>Test</h1>
                     </div>
                 </div>
