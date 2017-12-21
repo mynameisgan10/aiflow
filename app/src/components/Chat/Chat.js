c1075ff48934e28c983b2dac83057de164edafdd
gandeveloper <gan@Gans-New-Macbook-Pro.local>
2017-12-21 23:14:45 +0700

    chat layout completed

--- app/src/components/Chat/Chat.js
+++ app/src/components/Chat/Chat.js
@@ -1,73 +1,61 @@
 import React from 'react';
 import Aux from '../../HOC/aux/aux';
 
-const componentName = (props) => {
-    let align = 0;
-    const chat = props.chats.map((chat, index) => {
-        let classes;
-        if (index % 2 == 0) {
-            classes = ['text-bubble-container', 'text-bubble-container-left']
-        }else{
-            classes = ['text-bubble-container', 'text-bubble-container-right']
+let currentQuestion = 0;
+const Chat = (props) => {
+    let questionArray = [];
+    for (let index = 0; index < props.questions.length; index++) {
+        if (index === 0 && props.questions[index].response === null) {
+            questionArray.push(props.questions[0]);
+            break;
         }
+        if (index == 0 || props.questions[index - 1].response !== null) {
+            questionArray.push(props.questions[index]);
+            currentQuestion = index;
+        }
+    }
+
+    const chat = questionArray.map((chat, index) => {
         return (
-            <div className={classes.join(' ')}>
-                <div className="talk-bubble">
-                    <div className="talk-bubble-content align-text-left">
-                        <p>{chat}</p>
+            <Aux>
+                <div key={chat.id} className='text-bubble-container text-bubble-container-left'>
+                    <div className="talk-bubble">
+                        <div className="talk-bubble-content align-text-left">
+                            <p>{chat.question}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+                {chat.response ? (<div key={chat.id} className='text-bubble-container text-bubble-container-right'>
+                    <div className="talk-bubble">
+                        <div className="talk-bubble-content align-text-right">
+                            <p>{chat.response}</p>
+                        </div>
+                    </div>
+                </div>) : null}
+            </Aux>
         )
     })
     return (
         <Aux>
             <div className="ChatArea">
                 {chat}
-                <div className="text-bubble-container text-bubble-container-left">
-                    <div className="talk-bubble">
-                        <div className="talk-bubble-content align-text-left">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eaque perspiciatis magni obcaecati incidunt reiciendis culpa placeat deserunt, accusantium natus quam aut doloremque, quod maiores molestiae! Ipsum provident fugiat quisquam.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-bubble-container text-bubble-container-right">
-                    <div className="talk-bubble">
-                        <div className="talk-bubble-content align-text-right">
-                            <p>test jksadfj kl</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-bubble-container text-bubble-container-left">
-                    <div className="talk-bubble">
-                        <div className="talk-bubble-content align-text-left">
-                            <p>test jksadfj kl</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-bubble-container text-bubble-container-left">
-                    <div className="talk-bubble">
-                        <div className="talk-bubble-content align-text-left">
-                            <p>second Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod non. Ipsa ipsum recusandae id maxime vitae non, eaque quasi delectus aspernatur incidunt eius sequi laborum sed in dolore neque.</p>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div className="section">
-                <div className="field is-grouped">
-                    <p className="control is-expanded">
-                        <textarea class="textarea is-info" placeholder="e.g. Hello world" rows="1"></textarea>
-                    </p>
-                    <p className="control">
-                        <a className="button is-info is-medium">
-                            Send
+                <form onSubmit={(event) => props.submit(event,document.getElementById('input-area'), currentQuestion)}>
+                    <div className="field is-grouped">
+                        <p className="control is-expanded">
+                            <input class="textarea is-info" id="input-area" placeholder="e.g. Hello world" rows="1"></input>
+                        </p>
+                        <p className="control">
+                            <a className="button is-info is-medium" onClick={(event) => props.submit(event,document.getElementById('input-area'), currentQuestion)}>
+                                Send
                         </a>
-                    </p>
-                </div>
+                        </p>
+                    </div>
+                </form>
             </div>
         </Aux>
     );
 };
 
-export default componentName;
+export default Chat;
--- app/src/containers/NewApp/NewApp.js
+++ app/src/containers/NewApp/NewApp.js
@@ -6,12 +6,44 @@ import Chat from '../../components/Chat/Chat';
 class NewApp extends Component {
 
     state = {
-        testValue: [5,5,5]
+        questions: [
+            {
+                id: 1,
+                question: "What is the purpose of your app",
+                response: null
+            },
+            {
+                id: 2,
+                question: "Do you need a dedicated checkout addon",
+                response: null
+            },
+            {
+                id: 3,
+                question: "Do you need a delivery service",
+                response: null
+            }
+        ]
+    }
+    userRespond = (event,response, index) => {
+        event.preventDefault();
+        let updateState = { ...this.state };
+        updateState = {
+            questions: [...this.state.questions]
+        };
+        this.state.questions.map((question, index) => {
+            updateState.questions[index] = {
+                ...this.state.questions[index],
+            }
+        });
+        updateState.questions[index].response = response.value;
+        this.setState(updateState);
+        console.log(this.state);
+        response.value = '';
     }
     render() {
         return (
             <Aux>
-                <Chat chats={this.state.testValue} />
+                <Chat questions={this.state.questions} submit={this.userRespond} />
             </Aux >
 
         );
