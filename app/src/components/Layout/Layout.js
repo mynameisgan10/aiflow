013c849f68d7390749a6399b7be523c51e49cf7f
gandeveloper <gan@Gans-New-Macbook-Pro.local>
2018-01-03 23:31:53 +0800

    authentication state

--- app/src/components/Layout/Layout.js
+++ app/src/components/Layout/Layout.js
@@ -4,9 +4,11 @@ import AppDetail from '../../containers/AppDetail/AppDetail'
 import NewApp from '../../containers/NewApp/NewApp';
 import NavBar from '../Navbar/Navbar';
 import { Route, Switch,Redirect } from 'react-router-dom';
+
 class Layout extends Component {
     state = {
-        showModal: true
+        showModal: true,
+        isLoggedIn: true
     }
     appClick = () => {
         this.setState({
@@ -14,15 +16,23 @@ class Layout extends Component {
         })
     }
     render() {
-        return (
-            <div className="column is-12">
-                <NavBar />
-                <Switch>
+        let layout = (
+            <Switch>
                     <Route path="/" exact component={ProjectArea}></Route>
                     <Route path="/appdetail" exact component={AppDetail}></Route>
                     <Route path="/new" exact component={NewApp}></Route>
                     <Route path="*" render={() =><Redirect to="/"/>}/>
                 </Switch>
+        )
+        if(!this.state.isLoggedIn){
+            layout = (
+                <h1>Test</h1>
+            )
+        }
+        return (
+            <div className="column is-12">
+                <NavBar />
+                {layout}
             </div>
         );
     }
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
+
+
+//connecting to mongoDB server
+
+mongoose.connect('mongodb://localhost:27017/AIFLOW', {
+    useMongoClient: true
+});
+
+//connection to database was successful
+mongoose.connection.on('connected', () => {
+    console.log("connected to database");
+});
+
+//connection to the server failed
+
+mongoose.connection.on('error', (err) => {
+    console.log("database connected error" + err);
+});
 
 const port = 4700;
 
 
 //setting build file path
-app.use(express.static(path.join(__dirname,"app/build")));
+app.use(express.static(path.join(__dirname, "app/build")));
 
 //redirecting other links back to index.html
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,"app/build/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "app/build/index.html"));
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("server is running now!");
 })
